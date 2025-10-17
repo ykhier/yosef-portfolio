43cfb8318efc3fa35782deced639069e1fb32985
Yosef <yosef3125123@gmail.com>
2025-10-17 21:36:24 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react(),
-
+    react()
   ],
-  base: "/prtfolio",
+  base: '/prtfolio',
 })
